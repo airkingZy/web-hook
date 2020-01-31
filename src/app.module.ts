3fbd167d2d8ff55406a8e53432e14698412432d6
zystylish <zystylish@gmail.com>
2020-01-31 18:01:11 +0800

    chore: init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { MongooseModule } from "@nestjs/mongoose";
-let dbconfg = "mongodb://test:!QAZ%40wsx@127.0.0.1:27017/test";
-if (process.env.NODE_ENV === "production") {
-  dbconfg = "mongodb://stone:!QAZ%40wsx@127.0.0.1:27017/stonebook";
-} else {
-  console.log(process.env.NODE_ENV);
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+let dbconfg = 'mongodb://test:!QAZ%40wsx@127.0.0.1:27017/test';
+if (process.env.NODE_ENV === 'production') {
+  dbconfg = 'mongodb://stone:!QAZ%40wsx@127.0.0.1:27017/stonebook';
 }
 @Module({
   imports: [
     MongooseModule.forRoot(dbconfg, {
       useCreateIndex: true,
-      useNewUrlParser: true
-    })
+      useNewUrlParser: true,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
