7ef8fd9bf27ea8d05f94f61c2eb0b092b35f348b
zy <zystylish@gmail.com>
2020-02-01 21:58:12 +0800

    chore: init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,21 +4,8 @@ import { AppService } from './app.service';
 @Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   async getHello(): Promise<string> {
     return this.appService.getHello();
-  }
-  @Get('search/:name?')
-  async searchList(@Param('name') name: string): Promise<Object> {
-    return this.appService.searchList(name);
-  }
-  @Get('booklist/:url1?/:url2?')
-  async getBookDetail(
-    @Param('url1') url1: string,
-    @Param('url2') url2: string,
-  ): Promise<Object> {
-    const url = `${url1}/${url2}`;
-    return this.appService.getBookDetail(url);
   }
 }
